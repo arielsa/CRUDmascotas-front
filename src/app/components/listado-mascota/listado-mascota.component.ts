eb7ec6d7c6f7fffafb823db80217c22ade84c73e
ariel <santa.cruz.unla@gmail.com>
2023-01-17 20:37:55 -0300

    se agregaron mas objetos mascota para probar el paginator

--- src/app/components/listado-mascota/listado-mascota.component.ts
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -7,7 +7,17 @@ import { Mascota } from 'src/app/interfaces/mascota';
 
 
 const listMascotas: Mascota[] = [
-  { nombre: 'ringo', edad: 5, raza: 'boxer',color:'bayo',peso:10},
+  { nombre: 'ringo', edad: 5, raza: 'boxer',color:'bayo',peso:15},
+  { nombre: 'pepe', edad: 5, raza: 'golden',color:'dorado',peso:20},
+  { nombre: 'carl', edad: 4, raza: 'pitbul',color:'ambar',peso:30},
+  { nombre: 'boby', edad: 3, raza: 'boxer',color:'blanco',peso:9},
+  { nombre: 'rolfi', edad: 10, raza: 'caniche',color:'blanco',peso:6},
+  { nombre: 'ema', edad: 8, raza: 'yabran',color:'blanco',peso:11},
+  { nombre: 'tomy', edad: 5, raza: 'bulterrier',color:'marron',peso:18},
+  { nombre: 'terry', edad: 9, raza: 'boxer',color:'bayo',peso:10},
+  { nombre: 'gibson', edad: 13, raza: 'boxer',color:'bayo',peso:14},
+  { nombre: 'rayo', edad: 6, raza: 'ovejero',color:'negro',peso:8},
+  { nombre: 'preguntale', edad: 5, raza: 'boxer',color:'bayo',peso:20},
   
   
   
