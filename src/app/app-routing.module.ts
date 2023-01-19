a0298978772f8cc718299f9b0e824bb73cf7b54e
ariel <santa.cruz.unla@gmail.com>
2023-01-19 20:32:50 -0300

    ruteado con RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
 
-const routes: Routes = [];
+//componentes
+
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+
+const routes: Routes = [
+
+  {path:'listMacotas', component: ListadoMascotaComponent },
+  {path:'', redirectTo:'listMascotas', pathMatch: 'full' },
+  {path:'agregarMascota', component: AgregarEditarMascotaComponent },
+  { path:'verMascota/:id', component: VerMascotaComponent },
+  { path:'editarMascota/:id', component: AgregarEditarMascotaComponent  },
+  {path:'**', component: ListadoMascotaComponent },
+              
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
